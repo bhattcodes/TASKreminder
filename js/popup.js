cda0c88412342522538e2a5a450970038ced82c6
Aman Bhatt <whitexdevil09@gmail.com>
2021-01-18 04:22:57 +0530

    fixed the js file.
    delete option is now working properly. NO inliner fxn required anymore.. which created the problem.

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,6 @@
-// getting the user input in form of an object
 console.log("here in app");
 
-
+// fetch the data from local storage n show in the UO
 function show() {
     let obj = localStorage.getItem("tasks");
 
@@ -18,30 +17,34 @@ function show() {
         <tr>
         <td>${element.time}</td>
         <td>${element.task}</td>
-        <td class="del" id="${index}" "></td>
+        <td class="del" id="${index}" ></td>
         </tr>`;
 
     })
 
     document.getElementById("tasktable").innerHTML = html;
+    xyz();
 }
 
-function xyz() {
-    let i;
-    for (i = 0; i < document.getElementsByClassName("del").length; i++) {
+// below fxn adds event listeners to all the tasks
+function xyz() {                         
+    if (localStorage.getItem("tasks") != null) {
+        let i;
+        for (i = 0; i < document.getElementsByClassName("del").length; i++) {
+            console.log(`added in ${i}`);
+            document.getElementsByClassName("del")[i].addEventListener('click', (e) => {
 
-        document.getElementsByClassName(".del")[i].addEventListener('click', () => {
-            console.log("here! in deletetask", document.getElementsByClassName(".del")[i].getAttribute("id"));
-            // console.log(xt.getAttribute("id"));
-            // deletetask(xt.getAttribute("id"));
+                console.log("here! in deletetask",e.path[0].attributes.id.value);
+                deletetask(e.path[0].attributes.id.value);
 
-        })
+            })
+
+        }
 
     }
-
 }
 
-
+// deletes the clicked task by taking the id of the element
 function deletetask(index) {
     let obj = localStorage.getItem("tasks");
 
@@ -57,13 +60,13 @@ function deletetask(index) {
 
 }
 
-
+// when tick btn is clicked updates the local storage! 
 let tickbtn = document.querySelector(".tickbtn");
 tickbtn.addEventListener('click', () => {
 
     let obj = JSON.parse(localStorage.getItem("tasks"));
     if (obj == null) { obj = []; }
-
+    // getting the user input in form of an object
     obj.push({
         "time": document.getElementById("inputtime").value,
         "task": document.getElementById("inputtext").value,
